c71405a7952ef79d69ea36ffe2029aca21243bc6
Ashok <K.ashok1832@gamil.com>
2024-09-02 09:13:55 +0000

    Effect Hook and Rules of hooks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Scoreboard from './components/Scoreboard'
+
+import {GlobalStyle} from './styledComponents'
+
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    
+    <Scoreboard />
+  </>
+)
+
+export default App
